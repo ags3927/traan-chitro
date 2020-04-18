07c26f4368d5958a5e9b0668f4f08c15137c3214
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2020-04-18 20:10:19 +0600

    :hammer: Added process environment variables for vue and mongodb

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+//initialization
+process.env.VUE_APP_API_KEY = 'AIzaSyBdudQyn0ECon1ggxM-i3t4xhbQTVYAgLA';
+
+process.env.MONGODB_URI = 'mongodb+srv://arshuvo:ars123456789@tran-chitrodb-hge7d.mongodb.net/tran-chitro?retryWrites=true&w=majority';
+require('db/mongoose');
+
 let history = require('connect-history-api-fallback');
 let cors = require('cors');
 let bodyParser=require('body-parser');
@@ -7,6 +13,7 @@ let path = require('path');
 let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 
+//Routers
 let indexRouter = require('./routes');
 let apiRouter = require('./routes/api');
 
@@ -20,6 +27,8 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+//SPA Handling
 let staticFileMiddleware = express.static(path.join(__dirname+'/dist'));
 app.use(staticFileMiddleware);
 app.use(history());
