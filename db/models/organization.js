bf5e53575b37667738b2e671ba3a47a986160da8
SchrodingersCat041 <arshuvo2015@gmail.com>
2020-04-19 11:28:58 +0600

    :hammer: added validation to models

--- db/models/organization.js
+++ db/models/organization.js
@@ -1,13 +1,63 @@
-const mongoose = require('mongoose');
+let mongoose = require('mongoose');
+let urlRegex = require('url-regex')
 
-const Schema = mongoose.Schema;
+let {isEmail} = require('validator');
+let Schema = mongoose.Schema;
 
-const organizationSchema = new Schema({
-    name: {
+let validateURL = (url) => {
+    if (!urlRegex({exact: true}).test(url)){
+        throw new mongoose.Error('Invalid url');
+    }
+}
+
+let validateEmail = (email) => {
+    if (!isEmail(email)){
+        throw new mongoose.Error('Invalid email');
+    }
+}
+
+let organizationSchema = new Schema({
+    orgName: {
+        type: String,
+        unique: true,
+        index: 0,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    description: {
         type: String,
         required: true,
-        minLength: 1,
-        trim: true
+        minlength: 10
+    },
+    contact: {
+        phone: {
+            type: String,
+            required: true,
+            minlength: 14,
+            maxlength:14
+        },
+        email: {
+            type: String,
+            trim: true,
+            validate: {
+                validator: validateEmail
+            }
+        },
+        url: {
+            type: String,
+            trim: true,
+            validate: {
+                validator: validateURL
+            }
+        },
+        donate: {
+            type: String,
+            trim: true,
+            validate: {
+                validator: validateURL
+            }
+        }
     }
 });
 
--- db/mongoose.js
+++ db/mongoose.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 mongoose.connect(process.env.MONGODB_URI, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
+    useCreateIndex: true
     //autoIndex: false
 }).then(() => {
     console.log('You are connected to database.')
