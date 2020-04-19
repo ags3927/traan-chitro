637e01aa12ff4477a81c5c5553556666fc59f796
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2020-04-19 07:41:11 +0600

    :bug: No try cache issue fixed, response added for handleGETPins

--- controllers/activityController.js
+++ controllers/activityController.js
@@ -1,9 +1,21 @@
 const activityRepository = require('./../db/repository/activityRepository.js');
 const activityInterface = require('./../db/interfaces/activityInterface.js');
 
+/**
+ * Handles the GET request for all pins within bounds, optionally filtered based on certain criteria
+ * @param req
+ * @param res
+ * @returns {Promise<*>}
+ */
 const handleGETPins = async (req, res) => {
-    let body = req.body;
-    return await activityInterface.findActivitiesByBounds(body.bounds, body.filter);
+    try {
+        let body = req.body;
+        let result = await activityInterface.findActivitiesByBounds(body.bounds, body.filter);
+        return res.status(200).send(result);
+    } catch (e) {
+        console.log("ERROR in GET /api/pins\\Could not get pins");
+        return res.status(500).send(e.message);
+    }
 };
 
 module.exports = {handleGETPins};
