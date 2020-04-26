6494e31214980d0e2da5a7f1fd13dc16924be6d2
ags3927 <1605031@ugrad.cse.buet.ac.bd>
2020-04-26 15:51:29 +0600

    :bug: added authentication for all applicable routes and changed secret for auth middleware

--- routes/api.js
+++ routes/api.js
@@ -5,13 +5,13 @@ const activityController = require('./../controllers/activityController.js');
 const organizationController = require('./../controllers/organizationController.js');
 const authenticate = require('./../middlwares/authenticate.js');
 
-router.get('/pins', activityController.handleGETPins);
+router.get('/pins', authenticate.handleAuthentication, activityController.handleGETPins);
 
-router.get('/activities', activityController.handleGETActivitiesByCoordinates);
+router.get('/activities', authenticate.handleAuthentication, activityController.handleGETActivitiesByCoordinates);
 
 router.get('/orgs', organizationController.handleGETOrganizationNames);
 
-router.get('/orgdetails', organizationController.handleGETOrganizationDetails);
+router.get('/orgdetails', authenticate.handleAuthentication, organizationController.handleGETOrganizationDetails);
 
 router.post('/activity', authenticate.handleAuthentication, activityController.handlePOSTActivity);
 
